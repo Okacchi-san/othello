cf0082b661ae6540d0b5285a01f52f29a7e6331c
Yuki Takenaka <>
2021-02-02 17:16:31 +0900

    add function changeStoneRight

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
     <table>
-      <tr v-for="(y,yIndex) of stones" :key="yIndex">
-        <td v-for="(x,xIndex) of stones" :key="xIndex" v-on:click="onSelect(yIndex,xIndex)">
+      <tr v-for="(y, yIndex) of stones" :key="yIndex">
+        <td v-for="(x, xIndex) of stones" :key="xIndex" v-on:click="onSelect(yIndex, xIndex)">
           <Stone :type="stones[yIndex][xIndex]"></Stone>
         </td>
       </tr>
@@ -20,48 +20,70 @@ export default {
       size: 8,
       stones: [],
       turn: 1,
-    }
+    };
   },
   methods: {
-    onSelect: function(yIndex,xIndex) {
-      if(this.stones[yIndex][xIndex] !== 0) {
+    onSelect: function(yIndex, xIndex) {
+      if (this.stones[yIndex][xIndex] !== 0) {
         alert('すでに石が置かれています。');
         return;
       }
 
       this.stones[yIndex].splice(xIndex, 1, this.getCurrentStone());
-      this.turn ++;
+      this.changeStoneRight(xIndex, yIndex);
+      this.turn++;
     },
     getCurrentStone: function() {
       return this.turn % 2 !== 0 ? -1 : 1;
+    },
+  },
+  changeStoneRight: function(yIndex, xIndex) {
+    let reverseStones = [];
+    let flug = false;
+
+    for (let i = 1; i < this.size - xIndex; i++) {
+      if (this.stones[yIndex][xIndex + i] !== this.getCurrentStone()) {
+        reverseStones.push({ yIndex: yIndex, xIndex: xIndex + i });
+      } else if (this.stones[yIndex][xIndex + i] === 0) {
+        break;
+      } else {
+        flug = true;
+        break;
+      }
+    }
+
+    if (flug) {
+      reverseStones.map(i => {
+        return this.stones[i.yIndex].splice(i.xIndex, 1, this.getCurrentStone());
+      });
     }
   },
   created: function() {
     let stones = [];
-    for(let y = 0; y < this.size; y++) {
+    for (let y = 0; y < this.size; y++) {
       let row = [];
-      for(let x = 0; x < this.size; x++) {
-        row.push(0)
+      for (let x = 0; x < this.size; x++) {
+        row.push(0);
       }
       stones.push(row);
     }
     this.stones = stones;
     console.log(this.stones);
-  }
+  },
 };
 </script>
 
 <style scoped>
 table {
-    border-collapse: collapse;
+  border-collapse: collapse;
 }
 
 td {
-    border: 1px solid #999;
-    background: #f0f0f0;
-    width: 30px;
-    height: 30px;
-    text-align: center;
-    vertical-align: middle;
+  border: 1px solid #999;
+  background: #f0f0f0;
+  width: 30px;
+  height: 30px;
+  text-align: center;
+  vertical-align: middle;
 }
 </style>
